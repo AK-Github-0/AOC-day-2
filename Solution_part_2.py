79691c368630df634e8e200c234be6ed4f0f7bb1
Abdullah Khan <96420245+AK-hub-0@users.noreply.github.com>
2022-12-02 23:22:20 +0500

    Create Solution_part_2.py

---
+++ Solution_part_2.py
@@ -0,0 +1,23 @@
+dict = {
+  
+    "A X" : 3,
+    "A Y" : 4,
+    "A Z" : 8,
+  
+    "B X" : 1,
+    "B Y" : 5,
+    "B Z" : 9,
+  
+    "C X" : 2,
+    "C Y" : 6,
+    "C Z" : 7,
+
+}
+
+f = open("ak.txt", "r")
+sum1 = 0
+for line in f:
+  sum1 += dict3[line.split("\n")[0]]
+
+
+sum1
